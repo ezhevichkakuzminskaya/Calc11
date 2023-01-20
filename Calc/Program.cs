0191161428b27b50a973329e77525f4cc2430378
Viktoria <dbrf.repz@yandex.ru>
2023-01-20 12:07:31 +0300

    Addition

---
+++ Calc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int result;
+            Console.WriteLine("Введите число a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            result = a + b;
+            Console.WriteLine("Ответ:" + result);
+            Console.ReadKey();
+        }
+    }
+}
