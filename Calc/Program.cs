251f4a160d765e923ad5d3f3771c93145f83ec6e
Viktoria <dbrf.repz@yandex.ru>
2023-01-20 12:34:49 +0300

    Умножение

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -11,13 +11,15 @@ namespace Calc
         static void Main(string[] args)
         {
             int a, b;
-            int result;
+            int resAdd;
             Console.WriteLine("Введите число a:");
             a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите число b:");
             b = Convert.ToInt32(Console.ReadLine());
-            result = a + b;
-            Console.WriteLine("Ответ:" + result);
+            resAdd = a + b;
+            Console.WriteLine("Ответ:" + resAdd);
+            double resSub = a - b;
+            Console.WriteLine("Ответ:" + resSub);
             Console.ReadKey();
         }
     }
