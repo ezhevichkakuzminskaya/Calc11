51d8bf28321574ff9ebbac726864116ce9ee6447
ezhevichkakuzminskaya <123893108+ezhevichkakuzminskaya@users.noreply.github.com>
2023-01-30 10:18:18 +0300

    Commit2

---
+++ Calc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int resAdd;
+            Console.WriteLine("Введите число a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            resAdd = a + b;
+            Console.WriteLine("Ответ:" + resAdd);
+            double res = a * b;
+            Console.WriteLine("Ответ:" + res);
+            double resSub = a + b;
+            Console.WriteLine("Ответ:" + resSub);
+            double res1 = a / b;
+            Console.WriteLine("Ответ:" + res1);
+            double resproc = a % b;
+            Console.WriteLine("Ответ:" + resproc);
+            a++;
+            Console.WriteLine("Ответ:" + a);
+            double ressin = Math.Sin(a);
+            Console.WriteLine("Ответ:" + ressin);
+            double rescos = Math.Cos(a);
+            Console.WriteLine("Ответ:" + rescos);
+            Console.ReadKey();
+        }
+    }
+}
